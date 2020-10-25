28ad888747bc9ff0efffb21cc203523c84c17539
Mubashir-Hashmi <57247274+Mubashir-Hashmi@users.noreply.github.com>
2020-10-25 19:04:04 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SignUpScreen } from './screens/SignUpScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SignUpScreen/>
     </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
