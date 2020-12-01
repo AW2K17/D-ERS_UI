49757615ea322578ff671cacbfbff429b15df082
AW2K17 <73370422+AW2K17@users.noreply.github.com>
2020-12-01 02:02:03 -0800

    Add files via upload

--- Dashboard.js
+++ Dashboard.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 import { HomeScreen } from './HomeScreen';
 import { WorkoutScreen } from './WorkoutScreen';
@@ -16,7 +16,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 // import MyTabs from './BottomTab';
-import { NavigationContainer } from '@react-navigation/native';
+
+import { NavigationContainer, getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const HomeStack = createStackNavigator();
@@ -31,41 +32,91 @@ const LogoutStack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
-function HomeStackScreen() {
+
+const NavigationDrawerStructure = (props) => {
+  //Structure for the navigatin Drawer
+  const toggleDrawer = () => {
+    //Props to open/close the drawer
+    props.navigationProps.toggleDrawer();
+  };
+
   return (
-    <HomeStack.Navigator headerMode="none">
-      <HomeStack.Screen name="Home" component={HomeScreen} />
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity onPress={() => toggleDrawer()}>
+        {/*Donute Button Image */}
+        <Image
+          source={{
+            uri:
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/191px-Hamburger_icon.svg.png',
+          }}
+          style={{ width: 30, height: 25, marginLeft: 5 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const getHeaderTitle = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route);
+};
+
+
+const HomeStackScreen = ({ navigation }) => {
+  return (
+    <HomeStack.Navigator screenOptions={{ headerTitleAlign: 'center' }}>
+      <HomeStack.Screen name="HOME" component={HomeScreen} options={({ route }) => ({
+        headerTitle: getHeaderTitle(route),
+        headerLeft: () => (
+          <NavigationDrawerStructure
+            navigationProps={navigation}
+          />)
+      })} />
     </HomeStack.Navigator>
   );
 }
 
-function WorkoutStackScreen() {
+const WorkoutStackScreen = ({ navigation }) => {
   return (
-    <WorkoutStack.Navigator headerMode="none">
-      <WorkoutStack.Screen name="Workout" component={WorkoutScreen} />
+    <WorkoutStack.Navigator screenOptions={{
+      headerLeft: () => (
+        <NavigationDrawerStructure navigationProps={navigation} />
+      ),
+      headerTitleAlign: 'center'
+    }}>
+      <WorkoutStack.Screen name="WORKOUT" component={WorkoutScreen} />
     </WorkoutStack.Navigator>
   );
 }
 
-function DietStackScreen() {
+const DietStackScreen = ({ navigation }) => {
   return (
-    <DietStack.Navigator headerMode="none">
-      <DietStack.Screen name="Diet" component={DietScreen} />
+    <DietStack.Navigator screenOptions={{
+      headerLeft: () => (
+        <NavigationDrawerStructure navigationProps={navigation} />
+      ),
+      headerTitleAlign: 'center'
+    }}>
+      <DietStack.Screen name="DIET" component={DietScreen} />
     </DietStack.Navigator>
   );
 }
 
-function ProgressStackScreen() {
+const ProgressStackScreen = ({ navigation }) => {
   return (
-    <ProgressStack.Navigator headerMode="none">
-      <ProgressStack.Screen name="Progress" component={ProgressScreen} />
+    <ProgressStack.Navigator screenOptions={{
+      headerLeft: () => (
+        <NavigationDrawerStructure navigationProps={navigation} />
+      ),
+      headerTitleAlign: 'center'
+    }}>
+      <ProgressStack.Screen name="PROGRESS" component={ProgressScreen} />
     </ProgressStack.Navigator>
   );
 }
 
 function UsersStackScreen() {
   return (
-    <UsersStack.Navigator headerMode="none">
+    <UsersStack.Navigator headerMode='none'>
       <UsersStack.Screen name="Users" component={UsersScreen} />
     </UsersStack.Navigator>
   );
@@ -103,58 +154,59 @@ function LogoutStackScreen() {
   );
 }
 
-function TabsScreen () {
-  return(
+function TabsScreen() {
+  return (
     <Tab.Navigator headerMode="none"
-    initialRouteName="HomeStackScreen"
-    tabBarOptions={{
-      activeTintColor: '#e91e63',
-    }}
-  >
-    <Tab.Screen
-      name="Home"
-      component={HomeStackScreen}
-      options={{
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ color, size }) => (
-          <FontAwesome name="home" color={color} size={size} />
-        ),
+      initialRouteName="HomeStackScreen"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
       }}
-    />
-    <Tab.Screen
-      name="Workout"
-      component={WorkoutStackScreen}
-      options={{
-        tabBarLabel: 'Workout',
-        tabBarIcon: ({ color, size }) => (
-          <FontAwesome name="mars" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="Diet"
-      component={DietStackScreen}
-      options={{
-        tabBarLabel: 'Diet',
-        tabBarIcon: ({ color, size }) => (
-          <FontAwesome name="heartbeat" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="Progress"
-      component={ProgressStackScreen}
-      options={{
-        tabBarLabel: 'Progress',
-        tabBarIcon: ({ color, size }) => (
-          <FontAwesome name="tasks" color={color} size={size} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Workout"
+        component={WorkoutStackScreen}
+        options={{
+          tabBarLabel: 'Workout',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="mars" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Diet"
+        component={DietStackScreen}
+        options={{
+          tabBarLabel: 'Diet',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="heartbeat" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Progress"
+        component={ProgressStackScreen}
+        options={{
+          tabBarLabel: 'Progress',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="tasks" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 
 }
+
 
 
 const Drawer = createDrawerNavigator();
@@ -166,8 +218,8 @@ export default function Dashboard() {
     <NavigationContainer>
       {/* <Drawer.Navigator initialRouteName="Profile"> */}
       {/* <Drawer.Screen options={{ headerShown : false}}> */}
-      <Drawer.Navigator initialRouteName="Home" >
-        <Drawer.Screen name="Home" component={TabsScreen}  />
+      <Drawer.Navigator initialRouteName="Home" options={{ headerShown: false }}>
+        <Drawer.Screen name="Home" component={TabsScreen} options={{ headerShown: false }} />
         <Drawer.Screen name="Users" component={UsersStackScreen} />
         <Drawer.Screen name="Meals" component={MealsStackScreen} />
         <Drawer.Screen name="Workouts" component={WorkoutsStackScreen} />
